6c9e0fcf088ef3b6c78793735a6f9b8a1ba29cff
Renato Fraga <rofraga74@gmail.com>
2018-02-17 21:47:37 -0600

    new scenes

--- Swoosh/Swoosh/Views/BorderButton.swift
+++ Swoosh/Swoosh/Views/BorderButton.swift
@@ -12,8 +12,9 @@ class BorderButton: UIButton {
 
     override func awakeFromNib() {
         super.awakeFromNib()
-        layer.borderWidth = 3.0
+        layer.borderWidth = 2.0
         layer.borderColor = UIColor.white.cgColor
+        
     }
 
 }
